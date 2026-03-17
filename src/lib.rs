b4af087e5b755624d59f9c0d05a4857c5006e74c
LukasElias <lukaslundmajland@gmail.com>
2026-03-17 14:58:53 +0000

    WIP: Rewrote the code so I use a seq since it will be easier to repeatedly send the same pwm signal.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::singleton;
 use microbit::hal::{
     gpio::{Output, Pin, PushPull},
-    pwm::{Channel, Instance, Pwm},
+    pwm::{Channel, CounterMode, Instance, LoadMode, Pwm, PwmSeq},
     time::Hertz,
 };
 
-const SERVO_MAX_DEGREES: u16 = 180;
-
-pub struct ServoGroup<T: Instance> {
+pub struct ServoInitializinator<T: Instance> {
     pwm: Pwm<T>,
     len: usize,
+    max_degrees: f32,
 }
 
-impl<T: Instance> ServoGroup<T> {
-    pub fn new(pwm: Pwm<T>) -> Self {
-        Self { pwm, len: 0 }
+impl<T: Instance + core::fmt::Debug> ServoInitializinator<T> {
+    pub fn new(pwm: Pwm<T>, max_degrees: f32) -> Self {
+        pwm.set_counter_mode(CounterMode::Up);
+        pwm.set_load_mode(LoadMode::Individual);
+        pwm.loop_inf();
+
+        Self {
+            pwm,
+            len: 0,
+            max_degrees,
+        }
     }
 
     pub fn set_period(&self, freq: Hertz) {
@@ -33,28 +41,56 @@ impl<T: Instance> ServoGroup<T> {
         };
 
         self.pwm.set_output_pin(channel, pin);
+        self.pwm.enable_channel(channel);
+
         self.len += 1;
 
         Ok(channel)
     }
 
-    pub fn channel_set_degrees(&self, channel: Channel, degrees: u16) -> Result<(), ()> {
+    pub fn init(self) -> ServoSteerinator<T> {
+        let max_duty = self.pwm.max_duty();
+
+        let seq: &'static mut [u16; 4] = singleton!(: [u16; 4] = [2000, 0, 0, 0]).unwrap();
+
+        let seq_ptr = seq as *mut [u16; 4];
+
+        let pwm_seq = self.pwm.load(Some(&*seq), None::<&'static [u16; 4]>, true).unwrap();
+
+        ServoSteerinator {
+            _pwm_seq: pwm_seq,
+            seq_ptr,
+            max_duty,
+            max_degrees: self.max_degrees,
+        }
+    }
+}
+
+pub struct ServoSteerinator<T: Instance> {
+    _pwm_seq: PwmSeq<T, &'static [u16; 4], &'static [u16; 4]>,
+    seq_ptr: *mut [u16; 4],
+    max_duty: u16,
+    max_degrees: f32,
+}
+
+impl<T: Instance> ServoSteerinator<T> {
+    pub fn set_servo_degrees(&mut self, channel: Channel, degrees: f32) -> Result<(), ()> {
         let duty = self.degrees_to_duty(degrees)?;
 
-        self.pwm.set_duty_off(channel, duty);
-
-        self.pwm.enable_channel(channel);
+        unsafe {
+            (*self.seq_ptr)[channel as usize] = duty;
+        }
 
         Ok(())
     }
 
-    fn degrees_to_duty(&self, degrees: u16) -> Result<u16, ()> {
-        if degrees > SERVO_MAX_DEGREES {
+    pub fn degrees_to_duty(&self, degrees: f32) -> Result<u16, ()> {
+        if degrees > self.max_degrees {
             return Err(());
         }
 
-        let min_duty = self.pwm.max_duty() / 20;
+        let min_duty = self.max_duty as f32 / 20.0;
 
-        Ok(min_duty / SERVO_MAX_DEGREES * degrees + min_duty)
+        Ok((min_duty / self.max_degrees * degrees + min_duty) as u16)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,52 @@ use panic_probe as _;
 
 use cortex_m_rt::entry;
 
+use embedded_hal::delay::DelayNs;
+
 use microbit::{
     board::Board,
-    hal::{gpio::Level, pwm::Pwm, time::Hertz},
+    hal::{gpio::Level, pwm::Pwm, time::Hertz, timer::Timer},
 };
-use robot_arm::ServoGroup;
+
+use robot_arm::ServoInitializinator;
+
+const SERVO_MAX_DEGREES: f32 = 180.0;
 
 #[entry]
 fn main() -> ! {
     if let Some(board) = Board::take() {
+        let mut timer = Timer::new(board.TIMER0);
+
         let servopin = board.edge.e00.into_push_pull_output(Level::Low).degrade();
 
         let pwm = Pwm::new(board.PWM0);
 
-        let mut servo_group = ServoGroup::new(pwm);
+        let mut servo_initializinator = ServoInitializinator::new(pwm, SERVO_MAX_DEGREES);
 
-        servo_group.set_period(Hertz(50));
+        servo_initializinator.set_period(Hertz(50));
 
-        let channel_id = servo_group
+        let channel_id = servo_initializinator
             .new_servo(servopin)
             .expect("Added a servo too much");
 
-        servo_group.channel_set_degrees(channel_id, 35).unwrap();
+        let mut servo_steerinator = servo_initializinator.init();
 
-        loop {}
+        loop {
+            servo_steerinator.set_servo_degrees(channel_id, 0.0).unwrap();
+            timer.delay_ms(1000);
+
+            servo_steerinator.set_servo_degrees(channel_id, 45.0).unwrap();
+            timer.delay_ms(1000);
+
+            servo_steerinator.set_servo_degrees(channel_id, 90.0).unwrap();
+            timer.delay_ms(1000);
+
+            servo_steerinator.set_servo_degrees(channel_id, 135.0).unwrap();
+            timer.delay_ms(1000);
+
+            servo_steerinator.set_servo_degrees(channel_id, 180.0).unwrap();
+            timer.delay_ms(1000);
+        }
     }
     panic!("End");
 }
